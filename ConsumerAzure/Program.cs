36ce540d2ee16911df65fe6376a3092e40da7397
Heidi Kim Thomsen <1074310@ucn.dk>
2020-03-18 14:55:06 +0100

    FilteredData method works, and added comments.

--- ConsumerAzure/Program.cs
+++ ConsumerAzure/Program.cs
@@ -17,6 +17,7 @@ namespace ConsumerAzure {
         //TODO: make this better polling and not garbage as it is right now
         static void Main() {
             while (true) {
+                //Wait for 3 sek. 
                 Thread.Sleep(3000);
                 List<Location> data = GetData();
                 if (!(data.Count == 0)) {
@@ -25,6 +26,8 @@ namespace ConsumerAzure {
             }
         }
 
+        //This method gets data from the test data source provided by MapsPeople, and uses the method FilterData on that data. After that it returns a list of filtered data that has been converted to Internal Data Model by using the method ConvertFromJsonToInternalModel. 
+        //Return: Is a list of locations in the internal Data model format. 
         private static List<Location> GetData() {
             string jsonstr;
             var request = WebRequest.Create("https://mi-ucn-live-data.azurewebsites.net/occupancy?datasetid=6fbb3035c7e2436ba335edac") as HttpWebRequest;
@@ -32,18 +35,21 @@ namespace ConsumerAzure {
             using (StreamReader sr = new StreamReader(response.GetResponseStream())) {
                 jsonstr = sr.ReadToEnd();
             }
+            //The Data is in JSON, so it is deserialized so that it will be objects instead. 
             List<RootObject> sources = JsonConvert.DeserializeObject<List<RootObject>>(jsonstr);
 
             FilterData(sources);
             return ConvertFromJsonToInternalModel(filteredData);
         }
 
+        //This method Converts data in JSON format to the Internal Datamodel format. 
+        //Param: a list of json objects. 
+        //Return: A list of locations (internal)   
         private static List<Location> ConvertFromJsonToInternalModel(List<RootObject> sources) {
             List<Location> locations = new List<Location>();
             foreach (RootObject r in sources) {
                 Location location = new Location();
                 location.Id = r.SpaceRefId;
-
                 foreach (LastReport lr in r.LastReports) {
                     Source source = new Source();
                     string MotionDetected = lr.MotionDetected.ToString();
@@ -52,7 +58,6 @@ namespace ConsumerAzure {
                     source.State.Add("MotionDetected", MotionDetected);
                     source.State.Add("PersonCount", PersonCount);
                     source.State.Add("SignsOfLife", SignsOfLife);
-
                     source.Id = lr.Id;
                     source.Type = "Occupancy";
                     source.TimeStamp = lr.TimeStamp;
@@ -63,15 +68,26 @@ namespace ConsumerAzure {
             return (locations);
         }
 
+        //This method filters data so that it only keeps data that has been changed. 
+        //Param: Json String. 
         private static void FilterData(List<RootObject> rawData) {
+            //If the list filteredData is empty then add all the data from the list rawData. 
+            if(filteredData.Count == 0) {
+                filteredData.AddRange(rawData);
+            }
+            //A new list that can hold the changed data, before it is added to the list filteredData. 
             List<RootObject> temp = new List<RootObject>();
             foreach (RootObject r in rawData) {
                 foreach (RootObject rO in filteredData) {
+                    //Checks that it is the same source. 
                     if (r.Id.Equals(rO.Id)) {
                         foreach (LastReport l in r.LastReports) {
                             foreach (LastReport lr in rO.LastReports) {
+                                //Checks that it is the same source. 
                                 if (l.Id.Equals(lr.Id)) {
+                                    //If the property MotionDetected has changed. 
                                     if (!(l.MotionDetected.Equals(lr.MotionDetected))) {
+                                        //And if it doesn't already exists in the list temp - Then add it to the list. 
                                         if (!temp.Contains(r)) {
                                             temp.Add(r);
                                         }
@@ -92,18 +108,25 @@ namespace ConsumerAzure {
                     }
                 }
             }
-            filteredData.Clear();
-            filteredData.AddRange(temp);
+            //If there is something in the temp list, then the list filteredData will be cleared first, before the list temp is added. 
+            if (!(temp.Count == 0)) {
+                filteredData.Clear();
+                filteredData.AddRange(temp);
+            }
         }
 
+        //This method sends data to the Core Controller. 
+        //Param: Is a list of locations. 
         private static void SendData(List<Location> locations) {
             var client = new RestClient();
+            //TODO: 
             client.BaseUrl = new Uri("");
             string json = JsonConvert.SerializeObject(locations);
             var request = new RestRequest(Method.POST);
             request.AddParameter("application/json; charset=utf-8", json, ParameterType.RequestBody);
             request.RequestFormat = DataFormat.Json;
             var response = client.Execute(request);
+            Console.WriteLine(json);
         }
     }
 }
