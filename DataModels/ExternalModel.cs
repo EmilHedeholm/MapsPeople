a6cabf9daca66919da0c82d91ace9b528553d5b2
Zhiru Cao <1074160@ucn.dk>
2020-03-30 11:32:37 +0200

    external model  og convert class

---
+++ DataModels/ExternalModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels {
+    public class ExternalModel {
+        public string VenueId;
+        public string BuildingId;
+        public string FloorId;
+        public Source Source;
+    }
+}
