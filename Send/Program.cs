c92c60a43d723f0521ad35948b5a300f92e1fde9
Morten Kristensen <morten.m.kristensen@gmail.com>
2020-03-30 14:02:47 +0200

    Added functionality for sending messages to rabbitMQ via topic exchange.

---
+++ Send/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModels;
+
+namespace Send {
+    class Program {
+        static void Main(string[] args) {
+            List<State> states = new List<State>() { new State() { Id = "Raynolds", Property = "Uppercut", Value = "Over 9000" } };
+            Source sovs = new Source() { Id = "1234567", State = states, TimeStamp = DateTime.Now, Type = "Scouter" };
+            List<string> parentIds = new List<string>{ "Benny", "Jhonny", "Birger" };
+            ExternalHans hans = new ExternalHans() { ParentIds = parentIds, source = sovs };
+            List<ExternalHans> hanser = new List<ExternalHans>();
+            hanser.Add(hans);
+            SendMessage.SendUpdate(hanser);
+
+        }
+    }
+    public class ExternalHans {
+        public List<string> ParentIds { get; set; }
+        public Source source { get; set; }
+    }
+}
---
+++ Send/SendMessage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+using DataModels;
+
+namespace Send {
+    public class SendMessage {
+
+        public static void SendUpdate(List<ExternalHans> messages) { 
+            
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel()) {
+                channel.ExchangeDeclare(exchange: "Venue1",
+                                        type: "topic");
+                
+                foreach (var externalMessage in messages) {
+                    string routingKey = "";
+                    foreach (var parentId in externalMessage.ParentIds) {
+                       routingKey += parentId + ".";
+                    }
+                    var message = externalMessage.source;
+                    string json = JsonConvert.SerializeObject(externalMessage);
+                    var body = Encoding.UTF8.GetBytes(json);
+                    channel.BasicPublish(exchange: "Venue1",
+                                            routingKey: routingKey,
+                                            basicProperties: null,
+                                            body: body);
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+                }
+                
+            }
+        }
+    }
+
+
+}
