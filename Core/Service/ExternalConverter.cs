7e564b95a3cc38a0abeee5172ed99731ed3e99c7
Zhiru Cao <1074160@ucn.dk>
2020-03-30 19:15:21 +0200

    comment og rett equals "0" isted for !=null

--- Core/Service/ExternalConverter.cs
+++ Core/Service/ExternalConverter.cs
@@ -7,21 +7,26 @@ using DataModels;
 
 namespace Core.Service {
     public class ExternalConverter {
+
+        //This method Converts data from the location to the external Datamodel format. 
+        //Param: a Location object. 
+        //Return: A list of externalmodels. 
         public List<ExternalModel> Convert(Location location) {
             List<ExternalModel> externalModels = new List<ExternalModel>();
             ExternalModel externalModel = new ExternalModel();
             if (location != null) {
                 externalModel.LocationId = location.Id;
+                
                 if (location.Sources != null && location.Sources.Count() > 0) {
-                    for (int i = 0; i < location.Sources.Count(); i++) {
-                        if (location.ParentId != null) {
-                            externalModel.ParentIds = FindParentIds(location);
-                        }
+                    if (!location.ParentId.Equals("0")) {
+                        externalModel.ParentIds = FindParentIds(location);
+                    }
+                    for (int i = 0; i < location.Sources.Count(); i++) {                   
                         externalModel.Source = location.Sources[i];
                         externalModels.Add(externalModel);
                     }
                 } else {
-                    if (location.ParentId != null) {
+                    if (!location.ParentId.Equals("0")) {
                         externalModel.ParentIds = FindParentIds(location);
                     }
                     externalModels.Add(externalModel);
@@ -30,17 +35,19 @@ namespace Core.Service {
             return externalModels;
         }
 
+        //This method find all the parent IDs of a clocation
+        //Param: a Location object.
+        //Return: A string list of the parent IDs.
         private List<string> FindParentIds(Location location) {
             IDataAccess dataAccess = new DataAccess();
             List<string> parentIds = new List<string>();
-            Location l1 = dataAccess.GetLocationById(location.Id);
-            parentIds.Add(l1.ParentId);
-            if (l1.ParentId != null) {
+            parentIds.Add(location.ParentId);
+            Location l1 = dataAccess.GetLocationById(location.ParentId);
+            if (!l1.ParentId.Equals("0")) {
+                parentIds.Add(l1.ParentId);
                 Location l2 = dataAccess.GetLocationById(l1.ParentId);
-                parentIds.Add(l2.ParentId);
-                if (l2.ParentId != null) {
-                    Location l3 = dataAccess.GetLocationById(l2.ParentId);
-                    parentIds.Add(l3.ParentId);
+                if (!l2.ParentId.Equals("0")) {
+                    parentIds.Add(l2.ParentId);
                 }
             }
             return parentIds;
