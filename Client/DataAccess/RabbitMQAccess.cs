c84c9f0db89cfda782d905127bcf48ae7bf175db
Emil Hedeholm Sørensen <1074195@ucn.dk>
2020-04-02 09:31:36 +0200

    started working on consuming queues in the client

--- Client/DataAccess/RabbitMQAccess.cs
+++ Client/DataAccess/RabbitMQAccess.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Client.DataAccess {
     public class RabbitMQAccess {
-        public string GetData() {
-            throw new NotImplementedException();
+        public void GetData(string queueName) {
+            ConnectionFactory connectionFactory = new ConnectionFactory {
+                HostName = "localhost"
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.BasicQos(0, 1, false);
+
+            //var consumer = new EventingBasicConsumer(channel);
+            //consumer.Received += (sender, ea) => {
+            //    var body = ea.Body;
+            //    var message = Encoding.UTF8.GetString(body);
+            //    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            //};
+
+            channel.BasicConsume(queueName, false, consumer);
         }
     }
 }
