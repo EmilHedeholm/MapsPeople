bb96be29fd927b275ddd13643db291b23cc5c6aa
Zhiru Cao <1074160@ucn.dk>
2020-04-03 08:59:37 +0200

    sendcontroller

---
+++ Core/Controllers/SendController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DatabaseAccess;
+
+namespace Core.Controllers
+{
+    public class SendController : ApiController {
+        IDataAccess dataAccess = new DataAccess();
+
+    }
+    
+    
+}
