b118d0f45f2b74622e69c59553cb6a1af6945475
Emil Hedeholm Sørensen <1074195@ucn.dk>
2020-04-06 11:37:27 +0200

    made a service to get xml data from

---
+++ XMLDataSource/IXMLSource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace XMLDataSource {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IXMLSource {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "GetXMLData", ResponseFormat = WebMessageFormat.Xml)]
+        List<Location> GetXMLData();
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "XMLSource.ContractType".
+    [DataContract]
+    public class Location {
+        [DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public Source availability { get; set; }
+    }
+
+    [DataContract]
+    public class Source {
+        [DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public bool Available { get; set; }
+        [DataMember]
+        public DateTime TimeStamp { get; set; }
+    }
+}
+
---
+++ XMLDataSource/Service1.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace XMLDataSource {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class XMLSource : IXMLSource {
+        public List<Location> GetXMLData() {
+            List<Location> data = new List<Location>();
+            for (int i = 0; i < 500; i++) {
+                Location location = new Location();
+                location.Id = "" + i;
+                Source source = new Source();
+                source.Id = "" + i + 1;
+                Random random = new Random();
+                source.Available = random.Next() % 2 == 0 ? true : false;
+                source.TimeStamp = DateTime.Now;
+                location.availability = source;
+                data.Add(location);
+            }
+            return data;
+        }
+    }
+}
