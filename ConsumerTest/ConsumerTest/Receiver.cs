c36fc192dc0d43e1d158c69b176df690279a58ff
Zhiru Cao <1074160@ucn.dk>
2020-04-07 14:32:52 +0200

    consoleclient

--- ConsumerTest/ConsumerTest/Receiver.cs
+++ ConsumerTest/ConsumerTest/Receiver.cs
@@ -9,39 +9,38 @@ using RabbitMQ.Client.Events;
 
 namespace ConsumerTest {
     class Receiver {
-        public ExternalModel Consume() {
-
+        public void Consume() {
             ConnectionFactory factory = new ConnectionFactory();
-            // "guest"/"guest" by default, limited to localhost connections
             factory.HostName = "localhost";
-
             IConnection conn = factory.CreateConnection();
             IModel channel = conn.CreateModel();
+        TryAgain:
+            Console.WriteLine("Enter a queue ID");
+            string queueID = Console.ReadLine();
+            var response = channel.QueueDeclarePassive(queueID);
 
-            var response = channel.QueueDeclarePassive("287d4074d6c647a49f215fb1");
-            
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (sender, ea) => {
                 var body = ea.Body;
                 var message = Encoding.UTF8.GetString(body);
                 if (message != null) {
-                    //The message is converted from JSON to IEnumerable<Location>.
                     var deserializedMessage = JsonConvert.DeserializeObject<ExternalModel>(message);
                     foreach (var parentId in deserializedMessage.ParentIds) {
                         Console.Write(parentId + ", ");
                         Console.WriteLine();
                     }
-                    //Console.WriteLine(message + "\n\n");
-
                 }
                 channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
-            channel.BasicConsume(queue: "287d4074d6c647a49f215fb1",
+            channel.BasicConsume(queue: queueID,
                                  autoAck: false,
                                  consumer: consumer);
             Console.ReadLine();
-
-            return null;
+            goto TryAgain;
         }
     }
 }
+
+
+
+
