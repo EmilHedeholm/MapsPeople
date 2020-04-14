2295f187720b4a263f27791f74529beed522cf44
emil hedeholm sørensen <emilhedeholm@hotmail.com>
2020-04-14 12:03:34 +0200

    changed reciever in console klient from goto to while true

--- ConsumerTest/ConsumerTest/Receiver.cs
+++ ConsumerTest/ConsumerTest/Receiver.cs
@@ -7,36 +7,40 @@ using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-namespace ConsumerTest {
-    class Receiver {
-        public void Consume() {
+namespace ConsumerTest
+{
+    class Receiver
+    {
+        public void Consume()
+        {
             ConnectionFactory factory = new ConnectionFactory();
             factory.HostName = "localhost";
             IConnection conn = factory.CreateConnection();
             IModel channel = conn.CreateModel();
-        TryAgain:
-            Console.WriteLine("Enter a queue ID");
-            string queueID = Console.ReadLine();
-            var response = channel.QueueDeclarePassive(queueID);
+            while (true){
 
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (sender, ea) => {
-                var body = ea.Body;
-                var message = Encoding.UTF8.GetString(body);
-                if (message != null) {
-                    var deserializedMessage = JsonConvert.DeserializeObject<ExternalModel>(message);
-                    foreach (var parentId in deserializedMessage.ParentIds) {
-                        Console.Write(parentId + ", ");
-                        Console.WriteLine();
+                Console.WriteLine("Enter a queue ID");
+                string queueID = Console.ReadLine();
+                var response = channel.QueueDeclarePassive(queueID);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, ea) =>{
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    if (message != null) {
+                        var deserializedMessage = JsonConvert.DeserializeObject<ExternalModel>(message);
+                        foreach (var parentId in deserializedMessage.ParentIds){
+                            Console.Write(parentId + ", ");
+                            Console.WriteLine();
+                        }
                     }
-                }
-                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-            };
-            channel.BasicConsume(queue: queueID,
-                                 autoAck: false,
-                                 consumer: consumer);
-            Console.ReadLine();
-            goto TryAgain;
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: queueID,
+                                     autoAck: false,
+                                     consumer: consumer);
+                Console.ReadLine();
+            }
         }
     }
 }
