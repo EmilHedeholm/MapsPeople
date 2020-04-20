89ab17aa1ab2cfb7642ad616e2e4df5ab2e29847
Peanguin <Peanguin@users.noreply.github.com>
2020-04-20 09:40:03 +0200

    Added some stuff to MSSQL (Not tested)

--- DatabaseAccess/SQLDataAccess.cs
+++ DatabaseAccess/SQLDataAccess.cs
@@ -4,6 +4,7 @@ using DataModels;
 using System.Configuration;
 using System.Data.SqlClient;
 using Dapper;
+using System.Linq;
 
 namespace DatabaseAccess {
     public class SQLDataAccess : IDataAccess {
@@ -19,6 +20,7 @@ namespace DatabaseAccess {
 
         public void CreateLocation(Location location) {
             using (SqlConnection connection = new SqlConnection(conString)) {
+                findSourcesByLocationID(location);
                 var sql = "INSERT INTO LocationMP (id, parentId, externalId, consumerId) VALUES (@id, @parentId, @externalId, @consumerId);";
                 connection.Execute(sql, location);
 
@@ -26,18 +28,41 @@ namespace DatabaseAccess {
             }
         }
 
+        private List<Source> findSourcesByLocationID(Location location) {
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                return connection.Query<Source>("SELECT * FROM Source WHERE locationId =@locationId").ToList();
+            }
+        }
+
         private void CreateSource(Location location) {
             using (SqlConnection connection = new SqlConnection(conString)) {
+                var sources = findSourcesByLocationID(location);
+                foreach (var source in sources) {
+                    var foundStates = findStatesBySource(source);
+                    source.State.AddRange(foundStates);
+                }
+
+                var foundSources = "SELECT * FROM LocationMP WHERE locationId = @locationId, type = @type";
+                connection.Execute(foundSources, location);
+                var updateSource = "UPDATE Source SET timeStamp WHERE locationId = @locationId, type = @type";
+                connection.Execute(updateSource, location);
                 var sql = "INSERT INTO Source (locationId, type, timeStamp) VALUES (@locationId, @type, @timeStamp);";
                 connection.Execute(sql, location);
             }
         }
 
+        private List<State> findStatesBySource(Source source) {
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                return connection.Query<State>("Select * From State Where source = @locationId + @type").ToList();
+            } 
+        }
+
         private void CreateState(Source source) {
             using (SqlConnection connection = new SqlConnection(conString)) {
+                
                 var sql = "SELECT* FROM Source WHERE locationId = @locationId";
 
-                connection.Execute
+                connection.Execute;
             }
         }
 
@@ -50,23 +75,42 @@ namespace DatabaseAccess {
         }
 
         public List<Location> GetAllConnectedLocations(string id, List<Location> foundLocations) {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                var locations = connection.Query<Location>("select * from(select Id as Id union all select parentId) as X where Id in (@Id, @ParentId)").ToList();
+                foreach (var location in locations) {
+                    if (location.Id.Equals(id)) {
+                        foundLocations.Add(location);
+                    } else {
+                        foundLocations.Add(location);
+                        GetAllConnectedLocations(location.Id, foundLocations);
+                    }
+                }
+                return foundLocations;
+            }
         }
-
         public Location GetLocationByExternalId(string externalId) {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                return connection.Query<Location>("SELECT Id, parentId, externalId, consumerId FROM LocationMP WHERE externalId = @externalId", new { externalId }).SingleOrDefault();
+            }
         }
 
         public Location GetLocationById(string id) {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                return connection.Query<Location>("SELECT Id, parentId, externalId, consumerId FROM LocationMP WHERE id = @id", new { id }).SingleOrDefault();
+            }
         }
 
         public List<Location> GetLocations() {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                return connection.Query<Location>("SELECT Id, parentId, externalId, consumerId FROM LocationMP").ToList();
+            }
         }
 
         public void UpdateLocation(Location location) {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                var sql = "UPDATE LocationMP SET parentId = @parentId, externalId = @externalId, consumerId = @consumerId  WHERE id = @id;";
+                connection.Execute(sql, location);
+            }
         }
     }
 }
