6d04995a19a4b43c5ab644d89b1529dd6c74acac
Zhiru Cao <1074160@ucn.dk>
2020-04-23 15:02:08 +0200

    xml source kommenta

--- XMLSource2/XMLSource2/IService1.cs
+++ XMLSource2/XMLSource2/IService1.cs
@@ -7,7 +7,7 @@ using System.ServiceModel.Web;
 using System.Text;
 
 namespace XMLSource2 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // 
     [ServiceContract]
     public interface IXMLSource {
         [OperationContract]
@@ -15,8 +15,6 @@ namespace XMLSource2 {
         List<Location> GetXMLData();
     }
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "XMLSource.ContractType".
     [DataContract]
     public class Location {
         [DataMember]
--- XMLSource2/XMLSource2/Service1.svc.cs
+++ XMLSource2/XMLSource2/Service1.svc.cs
@@ -7,9 +7,10 @@ using System.ServiceModel.Web;
 using System.Text;
 
 namespace XMLSource2 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    
     public class Service1 : IXMLSource {
+        //This methods makes a new list of locations data which include source data 
+        // Return:A list of locations  
         public List<Location> GetXMLData() {
             List<Location> data = new List<Location>();
             for (int i = 1; i < 31; i++) {
