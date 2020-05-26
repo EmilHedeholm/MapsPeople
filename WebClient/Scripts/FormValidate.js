33503763c2dba72a13fac99c87b0a26cebc024de
emil hedeholm sørensen <emilhedeholm@hotmail.com>
2020-05-26 11:37:01 +0200

    added some css and javascript from old branch

---
+++ WebClient/Scripts/FormValidate.js
@@ -0,0 +1,31 @@
+﻿function validate() {
+    var error = false;
+    var errorText = "";
+    var errorName = false;
+    var numberOfErrors = 0;
+    var numberOfErrorsProcessed = 0;
+    // Queue ID
+    var inputQueue = "";
+    try {
+        inputQueue = document.chooseQueue.queueID.value;
+    }
+    catch (err) { return true; }
+    if (inputQueue.length < 1) {
+        error = true;
+        errorName = true;
+        errorText = "Input cannot be empty";
+        numberOfErrors++;
+    }
+    if (error) {
+        if (numberOfErrors > 1) {
+            errorText = "Search for a queue ID";
+            if (errorName) {
+                errorText += " ID";
+                numberOfErrorsProcessed++;
+            }
+        }
+        alert(errorText);
+        return false;
+    }
+    return true;
+}
